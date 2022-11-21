8cc1debb28d4c290d9961c8b50b1e7ff59f9fae5
Andres Badillo <r.andres.badillo@gmail.com>
2022-11-21 08:54:29 -0500

    First commit

---
+++ lib/src/pages/shoe_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ShoePage extends StatelessWidget {
+  const ShoePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('ShoePage'),
+      ),
+    );
+  }
+}
